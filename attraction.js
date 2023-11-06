52ee7334e68f24ca04a4ee5f43dcadb6bf4ac665
Mimi Wang <miny82525@gmail.com>
2023-11-06 20:00:14 +0800

    first commit

---
+++ attraction.js
@@ -0,0 +1,169 @@
+//分頁邏輯
+let currentPage = 1;
+function changePage(offset) {
+    currentPage += offset;
+    if (currentPage < 1) {
+        currentPage = 1;
+    }
+
+    // 調用獲取數據的函數
+    searchAttractions();
+}
+
+// 搜尋城市
+function searchAttractions() {
+    const city = document.getElementById("citySelect").value;
+    const spotName = document.getElementById("spotName").value;
+    const apiUrl = `https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/${city}?%24top=10&%24format=JSON`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // 把地名都找出來
+            const filteredAttractions = data.filter(attraction => {
+                return attraction.ScenicSpotName.includes(spotName);
+            });
+
+            // 分页逻辑
+            const itemsPerPage = 3;
+            const startIndex = (currentPage - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            const pageAttractions = filteredAttractions.slice(startIndex, endIndex);
+
+            // 輸出的格式
+            const table = document.getElementById("attractions");
+            table.innerHTML = `
+                <tr>
+                    <th>景點名稱</th>
+                    <th>開放時間</th>
+                    <th>地址</th>
+                </tr>
+            `;
+
+            // 輸出格式內容
+            pageAttractions.forEach(attraction => {
+                table.innerHTML += `
+                    <tr>
+                        <td class="attraction-name">${attraction.ScenicSpotName}</td>
+                        <td class="attraction-time">${attraction.OpenTime}</td>
+                        <td class="attraction-address">${attraction.Address}</td>
+                    </tr>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+// 清除所有景點的內容
+function searchAttractionsClear() {
+    const table = document.getElementById("attractions");
+    table.innerHTML = `
+        <tr>
+            <th>景點名稱</th>
+            <th>開放時間</th>
+            <th>地址</th>
+        </tr>
+    `
+}
+
+// 輸入景點名稱
+function showOnMap() {
+    const city = document.getElementById("citySelect").value;
+    const spotName = document.getElementById("spotName").value;
+    const apiUrl = `https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/${city}?%24top=10&%24format=JSON`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            //把地名都找出來
+            const filteredAttractions = data.filter(attraction => {
+                return attraction.ScenicSpotName.includes(spotName);
+            });
+            //輸出的格式
+            const table = document.getElementById("attractionShowOnMap");
+            table.innerHTML = `
+                <tr>
+                    <th>景點名稱</th>
+                    <th>開放時間</th>
+                    <th>地址</th>
+                </tr>
+            `;
+            //輸出格式內容
+            filteredAttractions.forEach(attraction => {
+                table.innerHTML += `
+                    <tr>
+                        <td>${attraction.ScenicSpotName}</td>
+                        <td>${attraction.OpenTime}</td>
+                        <td>${attraction.Address}</td>
+                    </tr>
+                `;
+                initMap(attraction.Position.PositionLat, attraction.Position.PositionLon, attraction.ScenicSpotName);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+function showOnMapClear() {
+    //清除特定景點的內容
+    const table = document.getElementById("attractionShowOnMap");
+    table.innerHTML = `
+        <tr>
+            <th>景點名稱</th>
+            <th>開放時間</th>
+            <th>地址</th>
+        </tr>
+    `;
+    // 刪除地圖
+    const mapElement = document.getElementById("mapid");
+    if (mapElement) {
+        mapElement.remove();
+    }
+}
+
+
+
+// 初始化地圖
+function initMap(latitude, longitude, name) {
+    const map = L.map('mapid').setView([latitude, longitude], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup(name)
+        .openPopup();
+}
+
+// default地圖
+// document.addEventListener("DOMContentLoaded", function() {
+//     const defaultLatitude = 25.033;
+//     const defaultLongitude = 121.565;
+//     const defaultName = "default";
+//     initMap(defaultLatitude, defaultLongitude, defaultName); 
+// })
+
+
+// footer
+const date = new Date();
+const currentDate = date.toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+});
+
+document.getElementById("copyright").innerHTML = `&copy ${currentDate} All Rights Reserved`;
